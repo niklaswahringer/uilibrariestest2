158c9823ad20069d1479543f1479e80fa1cb5ecd
Niklas Wahringer <n.wahringer@gmail.com>
2023-11-12 21:43:43 +0100

    add dashboard and auth components, add axios

---
+++ src/axios/axiosInterceptorInstance.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const axiosInterceptorInstance = axios.create({
+  baseURL: 'https://otp.dev/api/',
+});
+
+// Request interceptor
+axiosInterceptorInstance.interceptors.request.use(
+  (config) => {
+    // Modify the request config here (add headers, authentication tokens)
+    const accessToken = JSON.parse(localStorage.getItem("token") || '""');
+    
+    if (accessToken)
+      if (config.headers)
+        config.headers.token = accessToken;
+
+    return config;
+  },
+  (error) => {
+    // Handle request errors here
+
+    return Promise.reject(error);
+  }
+);
+// End of Request interceptor
+
+// Response interceptor
+axiosInterceptorInstance.interceptors.response.use(
+  (response) => {
+    // Modify the response data here
+
+    return response;
+  },
+  (error) => {
+    // Handle response errors here
+
+    return Promise.reject(error);
+  }
+);
+// End of Response interceptor
+
+export default axiosInterceptorInstance;
